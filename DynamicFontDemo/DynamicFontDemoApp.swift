87fb46801554392fcd9e1b949d6e6841788f2223
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-17 10:44:59 -0500

    Initial Commit

---
+++ DynamicFontDemo/DynamicFontDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFontDemoApp.swift
+//  DynamicFontDemo
+//
+//  Created by Mark Volkmann on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFontDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
