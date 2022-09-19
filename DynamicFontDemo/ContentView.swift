1a91531a71ed2c8a33b73239df9172097d3a957e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-19 16:56:50 -0500

    added comparison to fixed font sizes at default System Preferences text size

--- DynamicFontDemo/ContentView.swift
+++ DynamicFontDemo/ContentView.swift
@@ -8,22 +8,57 @@ struct ContentView: View {
             // These are in order from largest to smallest.
             // Groups are used to avoid issue with having > 10 views.
             Group {
-                Text("Hello, largeTitle!").font(.largeTitle)
-                Text("Hello, title!").font(.title)
-                Text("Hello, title2!").font(.title2)
-                Text("Hello, title3!").font(.title3)
+                HStack {
+                    Text("Hello, largeTitle X").font(.largeTitle)
+                    Text("X").font(.system(size: 35))
+                }
+                HStack {
+                    Text("Hello, title X").font(.title)
+                    Text("X").font(.system(size: 28))
+                }
+                HStack {
+                    Text("Hello, title2 X").font(.title2)
+                    Text("X").font(.system(size: 23))
+                }
+                HStack {
+                    Text("Hello, title3 X").font(.title3)
+                    Text("X").font(.system(size: 20))
+                }
             }
 
             Group {
-                // .headline is the same size as .body but is bold.
-                Text("Hello, headline!").font(.headline)
-                Text("Hello, body!").font(.body)
-                Text("Hello, callout!").font(.callout)
-                Text("Hello, subheadline!").font(.subheadline)
-                Text("Hello, footnote!").font(.footnote)
-                Text("Hello, caption!").font(.caption)
-                Text("Hello, caption2!").font(.caption2)
+                HStack {
+                    // headline is the same size as body, but bold.
+                    Text("Hello, headline X").font(.headline)
+                    Text("X").font(.system(size: 17))
+                }
+                HStack {
+                    Text("Hello, body X").font(.body)
+                    Text("X").font(.system(size: 17))
+                }
+                HStack {
+                    Text("Hello, callout X").font(.callout)
+                    Text("X").font(.system(size: 16))
+                }
+                HStack {
+                    Text("Hello, subheadline X").font(.subheadline)
+                    Text("X").font(.system(size: 15))
+                }
+                HStack {
+                    Text("Hello, footnote X").font(.footnote)
+                    Text("X").font(.system(size: 13))
+                }
+                HStack {
+                    Text("Hello, caption X").font(.caption)
+                    Text("X").font(.system(size: 12))
+                }
+                HStack {
+                    Text("Hello, caption2 X").font(.caption2)
+                    Text("X").font(.system(size: 11))
+                }
             }
+
+            Spacer()
         }
         .padding()
     }
